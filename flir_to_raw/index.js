922886ce7b1633d7d4531f714283e95b50ca1b75
avmey <avmey98@gmail.com>
2020-12-08 14:59:23 -0500

    testing

--- flir_to_raw/index.js
+++ flir_to_raw/index.js
@@ -2,7 +2,7 @@
 const execFile = require('child_process').execFile;
 const exiftool = require('dist-exiftool');
 const fs = require('fs');
-const gm = require('graphicsmagick');
+const gm = require('gm');
 const im = require('imagemagick');
 
 // Function triggered by new blob in "uploads" folder
